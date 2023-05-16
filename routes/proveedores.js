fee09e54e8769c1263b2d3cbe921c37eb9bdec04
Axel Lopez <axellopez2002@gmail.com>
2023-05-15 18:59:30 -0600

    se arreglo proveedores busqueda por id

--- routes/proveedores.js
+++ routes/proveedores.js
@@ -3,14 +3,13 @@ var router = express.Router();
 const controlador = require('../controlador/ProveedoresControlador.js');
 /* todos los proveedores */
 router.get('/',controlador.Proveedores);
-// un provedor por su id
-router.post('/:id',controlador.BuscarProveedorId);
 // nuevo provedor
 router.post('/nuevo',controlador.NuevoProveedor);
 // modificar proveedor
 router.put('/actualizar/:id',controlador.ModificarProveedor);
 // desactivar proveedor
 router.put('/desactivar/:id',controlador.DesactivarProveedor);
-
+// un provedor por su id
+router.post('/:id',controlador.BuscarProveedorId);
 
 module.exports = router;
