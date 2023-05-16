ce397a6b86ad478484389ef5e394304b5ed9914e
Axel Lopez <axellopez2002@gmail.com>
2023-05-15 20:52:15 -0600

    se arreglo CORS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require(cors);
+var cors = require("cors");
 
 var connectarDB = require("./config/db");
 
