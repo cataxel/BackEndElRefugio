d22fb6e4079cd760899ef349f5e37d54477e7a70
Axel Lopez <axellopez2002@gmail.com>
2023-05-16 19:54:20 -0600

    se quito cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var medicamentosRouter = require('./routes/Medicamentos');
 
 var app = express();
 
-app.use(cors);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
