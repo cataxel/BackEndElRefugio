c966c1c32695ae6ff293c5ba378ff393112fa93f
Axel Lopez <axellopez2002@gmail.com>
2023-05-18 18:09:09 -0600

    se arreglo usuarios id

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,12 @@ const controlador = require('../controlador/usuarioControlador.js');
 // empleados
 /* GET users listing. */
 router.get('/', controlador.Usuario);
-// buscar usuario por id
-router.post('/:id',controlador.BuscarUsuarioId);
 // añadir usuario
 router.post('/nuevo', controlador.CrearUsuario);
 // actualizar usuario
 router.put('/actualizar/:id', controlador.ActualizarUsuario);
 // desactivar los usuarios
 router.put('/desactivar/:id', controlador.DesactivarUsuario);
-
+// buscar usuario por id
+router.post('/:id',controlador.BuscarUsuarioId);
 module.exports = router;
