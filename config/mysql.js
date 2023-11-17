d53114da14b6022a3e54cab5a358f393369e742a
Axel Lopez <axellolopez2002@gmail.com>
2023-11-17 17:59:14 -0600

    implementacion BD en la nube

--- config/mysql.js
+++ config/mysql.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
+const fs = require('fs');
 const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl:{
+    ca: fs.readFileSync(process.env.DB_SSL_CA)
+  }
 });
   
   connection.connect(function (error) {
@@ -15,17 +19,5 @@ const connection = mysql.createConnection({
     }
     console.log('Connection established');
   });
-
-  /*
-  const connectDB = async () => {
-
-    try {
-      await connection.connect; // Obtiene una conexión asincrónica
-      console.log('MySQL connected');
-    } catch (error) {
-      console.error('Error:', error.message);
-      process.exit(1);
-    }
-  };*/
   
   module.exports = connection;
