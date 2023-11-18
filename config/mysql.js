9011041c5a667345bd2f18778eceae4420974218
Axel Lopez <axellolopez2002@gmail.com>
2023-11-18 16:47:06 -0600

    prueba db host y port directo

--- config/mysql.js
+++ config/mysql.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: 'farmadb-do-user-14009899-0.c.db.ondigitalocean.com',
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: '25060',
 });
   
   connection.connect(function (error) {
     if (error) {
       console.log('Error connection database :',error);
-      
       return;
     }
     console.log('Connection established');
