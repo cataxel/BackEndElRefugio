394811760a0ff6da11d86476573a8ded5162f785
Axel Lopez <axellolopez2002@gmail.com>
2023-11-18 17:24:08 -0600

    se puso directamente las claes

--- config/mysql.js
+++ config/mysql.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: 'farmadb-do-user-14009899-0.c.db.ondigitalocean.com',
+  user: 'doadmin',
+  password: 'AVNS_KWGtIV-_1csnQJ42Nk',
+  database: 'FarmaExpress',
+  port: '25060',
 });
-  
   connection.connect(function (error) {
     if (error) {
       console.log('Error connection database :',error);
