0a6d1f12dd46272411838f379e8eb6e4adb5090c
Axel Lopez <axellolopez2002@gmail.com>
2023-11-17 19:42:02 -0600

    Actualizacion se quito fs

--- config/mysql.js
+++ config/mysql.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const fs = require('fs');
 const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -7,9 +6,6 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl:{
-    ca: fs.readFileSync(process.env.DB_SSL_CA)
-  }
 });
   
   connection.connect(function (error) {
